6372e378b493060862674e4bf055590a1d70d759
martynas <martynas.gelumbauskas@gmail.com>
2019-09-13 13:19:23 -0700

    appdrawer

--- app/src/main/java/com/example/kablys/WeatherActivity.java
+++ app/src/main/java/com/example/kablys/WeatherActivity.java
@@ -1,13 +1,44 @@
 package com.example.kablys;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 public class WeatherActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private static long id = LoginActivicty.getID();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.open_navigation, R.string.close_navigation);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        Toast.makeText(WeatherActivity.this, Long.toString(id),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public  void onBackPressed()
+    {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
